38deba774eef57f75f2eafb2af2f017094591046
artemkosnikov <Boxberry123>
2021-06-15 17:34:39 +0300

    fixes

--- src/Cart.ts
+++ src/Cart.ts
@@ -11,27 +11,17 @@ export default class Cart {
         return [...this.items];
     }
     sumAll(): number {
-        let sum = 0;
-        for( let elem of this.items) {
-            sum += elem.price
-        }
-        return sum
+        return this.items.reduce((sum:number, price: Buyable) => sum + price.price, 0)
     }
     discountSum(discount: number): number {
-        let sum = 0;
-        for( let elem of this.items) {
-            sum += elem.price
-        }
-        return (sum - discount)
+        return (this.sumAll() - discount)
     }
     removeItem(id: number): Buyable[] {
-        for( let elem of this.items) {
-            if (elem.id === id) {
-                let index = this.items.indexOf(elem);
-                if (index > -1) {
-                    this.items.splice(index, 1);
-                }
-            }
+       let itemForRemove = this.items.find(item => item.id == id);
+       // @ts-ignore
+        let index = this.items.indexOf(itemForRemove);
+        if (index > -1) {
+            this.items.splice(index, 1)
         }
         return this.items
     }
